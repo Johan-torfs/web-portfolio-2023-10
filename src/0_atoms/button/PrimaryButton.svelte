e6b80e696d8e741440a0fe33b12401be958ae678
Johan Torfs <j.t.johan.torfs@gmail.com>
2023-10-26 21:43:04 +0200

    testing ui for contact form

--- src/0_atoms/button/PrimaryButton.svelte
+++ src/0_atoms/button/PrimaryButton.svelte
@@ -10,6 +10,7 @@
     data-blok-c={storyblokData?.['data-blok-c']} data-blok-uid={storyblokData?.['data-blok-uid']}
     class={className}
     type="submit"
+    disabled={$inputs["__submitting"]}
 >
     {#if !!icon?.type || $inputs["__submitting"]}
         <i class={$inputs["__submitting"] ? "fa-spinner fa-spin" : [icon.type, icon.icon].join(' ')} aria-hidden="true"></i>
