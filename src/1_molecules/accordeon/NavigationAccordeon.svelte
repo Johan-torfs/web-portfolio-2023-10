894bd3458cc66c63e7830969f51e4324ca8793cd
Johan Torfs <j.t.johan.torfs@gmail.com>
2023-10-26 17:48:41 +0200

    fix to accordeon layout

--- src/1_molecules/accordeon/NavigationAccordeon.svelte
+++ src/1_molecules/accordeon/NavigationAccordeon.svelte
@@ -11,16 +11,16 @@ function toggle() {
 
 </script>
 
-<button class={className + ' transition'} data-blok-c={storyblokEditableData['data-blok-c']} data-blok-uid={storyblokEditableData['data-blok-uid']} on:click={toggle}>
-    <div class={"flex justify-between items-center" + (open ? ' mb-8' : '')}>
+<div class={className + ' transition'} data-blok-c={storyblokEditableData['data-blok-c']} data-blok-uid={storyblokEditableData['data-blok-uid']}>
+    <button class={"flex justify-between items-center text-start w-full" + (open ? ' mb-8' : '')} on:click={toggle}>
         <slot name="title"/>
-        <svg class={"h-6 w-6 fill-current transition-transform" + ( open ? ' rotate-180' : '')} viewBox="0 0 20 20">
+        <svg class={"h-6 w-6 fill-current transition-transform flex-shrink-0" + ( open ? ' rotate-180' : '')} viewBox="0 0 20 20">
             <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"></path>
         </svg>
-    </div>
+    </button>
     {#if open}
-    <div class="text-left" transition:slide>
+    <div transition:slide>
         <slot name="content"/>
     </div>
     {/if}
-</button>
+</div>
